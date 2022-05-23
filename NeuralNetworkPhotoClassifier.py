aba9189cd22cdb8a68d3d384381a34c1c9f0b8e5
sevarikaboris <sevarikaboris@gmail.com>
2022-05-23 16:53:21 -0400

    okay now?

--- NeuralNetworkPhotoClassifier.py
+++ NeuralNetworkPhotoClassifier.py
@@ -1,4 +1,5 @@
 import os
+from typing import Counter
 import numpy as np
 import scipy
 from keras.models import load_model
@@ -22,70 +23,83 @@ for experiment_dir in os.listdir(basepath):
     experiment_dir += "/"
     print("processing: " + experiment_dir)
 
-    for videos_dir in os.listdir(basepath + experiment_dir):
-        if(not os.path.isdir(basepath + experiment_dir + videos_dir)):
+    if not dedrift:
+        file_name = '/tracked_particles.mat'
+    elif dedrift:
+        file_name = '/tracked_particles_dedrift.mat'
+
+    mat_file = loadmat(basepath + experiment_dir + file_name)
+    concat_ptcl2 = mat_file['concat_ptcl2'] #read the file
+    
+    video_folder_list = list()
+    counter_fov = 0
+    for videos_fov_dir in os.listdir(basepath + experiment_dir):
+        if(not os.path.isdir(basepath + experiment_dir + videos_fov_dir)):
             continue
-
-        videos_dir += "/"
-        print("videos: "  + videos_dir)
-
-        if not dedrift:
-            file_name = '/tracked_particles.mat'
-        elif dedrift:
-            file_name = '/tracked_particles_dedrift.mat'
-
-        mat_file = loadmat(basepath + experiment_dir + file_name)
-        concat_ptcl2 = mat_file['concat_ptcl2'] #read the file
-        #savename = basepath + experiment_dir + videos_dir
         
-        row, col = concat_ptcl2.shape               #get rows and cols of mat_file
-        table_with_result = np.zeros(shape=(row,6)) #initializing resulting table. it is faster to fill the array with zeros beforehand, than appending and copying tables
-        table_with_result_index = 0                 #navigating index 
+        number = counter_fov
+        if(counter_fov == 1):
+            number = 1
+        elif(counter_fov == 2):
+            number = 10
+        else:
+            number = counter_fov -1
 
-        for concat_row in concat_ptcl2:
-            picture_number = int(concat_row[2])
-            if picture_number <10:
-                number = '_000' + str(picture_number)
-            elif picture_number <100:
-                number = '_00' + str(picture_number)
-            else:
-                number = '_0' + str(picture_number)
+        video_folder_list.append("fov" + number)
 
-            img2Name = basepath + experiment_dir + videos_dir +  'fov1' +number + '.tif'
-            img = Image.open(img2Name)
 
-            #left, up, right, bottom
-            heigth = 40
-            width = 40
-            left = concat_row[0]-(width/2)
-            top = concat_row[1]-(heigth/2)
-            right = left + width
-            bottom =top + heigth
-            
-            imgSel = img.crop((left, top, right, bottom))
-            imgSel = imgSel.resize((224, 224), Image.LANCZOS)
-            imgSel = np.asarray(imgSel).reshape(1, 224, 224, 3)
-            image = preprocess_input(imgSel)
-            
-            X_after_vgg = vgg16_model.predict(image)
-            X_after_vgg.shape
+    #"E:\Particle Set 2\100 nm - C 1.0 - 1\fov1\fov1_0001.tif"
+    
+    row, col = concat_ptcl2.shape               #get rows and cols of mat_file
+    table_with_result = np.zeros(shape=(row,6)) #initializing resulting table. it is faster to fill the array with zeros beforehand, than appending and copying tables
+    table_with_result_index = 0                 #navigating index 
 
-            
-            x = model.predict(X_after_vgg)
-            print("particle id: "+ str(concat_row[3]))
 
-            #if x >= 0.9: 
-            #    print("is Particle")
-            #else : 
-            #    print("not a Particle")
 
-            for i in range(5):
-                table_with_result[table_with_result_index][i] = concat_row[i]
-            table_with_result[table_with_result_index][5] = x
-            table_with_result_index += 1
-            
-            
-        scipy.io.savemat('result_mat'+str(concat_row[2])+'.mat',mdict={'concat_ptcl2': table_with_result})
+    for concat_row in concat_ptcl2:
+        picture_number = int(concat_row[2])
+        if picture_number <10:
+            number = '_000' + str(picture_number)
+        elif picture_number <100:
+            number = '_00' + str(picture_number)
+        else:
+            number = '_0' + str(picture_number)
+
+        img2Name = basepath + experiment_dir + video_folder_list[int(concat_row[2])-1] + "/" +  video_folder_list[int(concat_row[2])-1] + number + '.tif'
+        img = Image.open(img2Name)
+
+        #left, up, right, bottom
+        heigth = 40
+        width = 40
+        left = concat_row[0]-(width/2)
+        top = concat_row[1]-(heigth/2)
+        right = left + width
+        bottom =top + heigth
+        
+        imgSel = img.crop((left, top, right, bottom))
+        imgSel = imgSel.resize((224, 224), Image.LANCZOS)
+        imgSel = np.asarray(imgSel).reshape(1, 224, 224, 3)
+        image = preprocess_input(imgSel)
+        
+        X_after_vgg = vgg16_model.predict(image)
+        X_after_vgg.shape
+
+        
+        x = model.predict(X_after_vgg)
+        print("particle id: "+ str(concat_row[3]))
+
+        #if x >= 0.9: 
+        #    print("is Particle")
+        #else : 
+        #    print("not a Particle")
+
+        for i in range(5):
+            table_with_result[table_with_result_index][i] = concat_row[i]
+        table_with_result[table_with_result_index][5] = x
+        table_with_result_index += 1
+        
+        
+    scipy.io.savemat('result_mat'+str(concat_row[2])+'.mat',mdict={'concat_ptcl2': table_with_result})
             
             
 
