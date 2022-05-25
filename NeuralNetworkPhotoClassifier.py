6374154f94fc788a51c0874030b3b1dce5b65b4d
JeromeBollinger <jerome.f.bollinger@gmail.com>
2022-05-25 22:32:00 +0200

    processing lines test

--- NeuralNetworkPhotoClassifier.py
+++ NeuralNetworkPhotoClassifier.py
@@ -10,28 +10,21 @@ import time
 from multiprocessing import Pool
 import random
 import ssl
+
 ssl._create_default_https_context = ssl._create_unverified_context
-
-#test commit
-
-basepath = "E:/Analyzed/" #path where experiments are located
-model = load_model("C:/Users/sevar/OneDrive/Documents/OneNote/OneDrive/Архив/Research/MIT Project/Microrheology/MPT - new/BorisBoris.h5")
 vgg16_model = VGG16(include_top=False, input_shape=(224, 224, 3))
 
-#change this value
+
+#change these values
+basepath = "E:/Analyzed/" #path where experiments are located
+model = load_model("C:/Users/sevar/OneDrive/Documents/OneNote/OneDrive/Архив/Research/MIT Project/Microrheology/MPT - new/BorisBoris.h5") #path of used h5 model
 dedrift = False
-core_count = 4
-cores_in_use = 0
-file_dirs = list()
-experiment_dir = ""
 probability = 0.1 
 
-def transpose_matrix(matrix):
-    return list(map(list, zip(*matrix)))
+
+
 
 def evaluate_table(mat_file_path):
-    for mat_file_path in os.listdir(basepath):
-        mat_file_path += "/"
     print("processing: " + mat_file_path)
 
     if not dedrift:
@@ -69,70 +62,70 @@ def evaluate_table(mat_file_path):
 
 
     for concat_row in concat_ptcl2:
-
-        if(random.random()>probability):
-            for i in range(6):
-                table_with_result[table_with_result_index][i] = concat_row[i]
-            table_with_result[table_with_result_index][5] = 10.0
-            table_with_result_index += 1
-            continue
-
-        picture_number = int(concat_row[2])
-        if picture_number <10:
-            number = '_000' + str(picture_number)
-        elif picture_number <100:
-            number = '_00' + str(picture_number)
-        else:
-            number = '_0' + str(picture_number)
-
-        img2Name = basepath + mat_file_path + video_folder_list[int(concat_row[4])-1] + "/" +  video_folder_list[int(concat_row[4])-1] + number + '.tif'
-        img = Image.open(img2Name)
-        # img.save("C:/Users/sevar/OneDrive/Radna površina/Save/Test - 1.png","PNG")
-
-        #left, up, right, bottom
-        heigth = 40
-        width = 40
-        left = concat_row[0]-(width/2)
-        top = concat_row[1]-(heigth/2)
-        right = left + width
-        bottom =top + heigth
-        
-        imgSel = img.crop((left, top, right, bottom))
-        
-
-        imgSel = imgSel.resize((224, 224), Image.LANCZOS)
-        # imgSel.save("C:/Users/sevar/OneDrive/Radna površina/Save/Test - 2.jpg")
-        
-        imgSel = np.asarray(imgSel).reshape(1, 224, 224, 3)
-        image = preprocess_input(imgSel)
-        
-        X_after_vgg = vgg16_model.predict(image)
-        X_after_vgg.shape
-
-        
-        x = model.predict(X_after_vgg)
-        print("particle id: "+ str(concat_row[3]))
-
-        print(x)
-
-        for i in range(5):
-            table_with_result[table_with_result_index][i] = concat_row[i]
-        table_with_result[table_with_result_index][5] = x
-        table_with_result_index += 1
+        p = Pool()
+        result = p.starmap(process_mat_file_row, [table_with_result, 
+                        table_with_result_index, 
+                        concat_row,
+                        mat_file_path,
+                        video_folder_list])
+        p.close()
+        p.join()   
         
         
     scipy.io.savemat(basepath + mat_file_path + 'result_mat'+'.mat',mdict={'concat_ptcl': table_with_result})
 
+def process_mat_file_row(table_with_result, 
+                        table_with_result_index, 
+                        concat_row,
+                        mat_file_path,
+                        video_folder_list):
+
+    if(random.random()>probability):
+        for i in range(6):
+            table_with_result[table_with_result_index][i] = concat_row[i]
+            table_with_result[table_with_result_index][5] = 10.0
+            table_with_result_index += 1
+            continue
+
+    picture_number = int(concat_row[2])
+    if picture_number <10:
+        number = '_000' + str(picture_number)
+    elif picture_number <100:
+        number = '_00' + str(picture_number)
+    else:
+        number = '_0' + str(picture_number)
+
+    img2Name = basepath + mat_file_path + video_folder_list[int(concat_row[4])-1] + "/" +  video_folder_list[int(concat_row[4])-1] + number + '.tif'
+    
+    x = process_image(img2Name, concat_row[0], concat_row[1], 40)
+
+    for i in range(5):
+        table_with_result[table_with_result_index][i] = concat_row[i]
+    table_with_result[table_with_result_index][5] = x
+    table_with_result_index += 1
+
+def process_image(img2Name, x_coord, y_coord, crop_image_size):
+    img = Image.open(img2Name)
+    left = x_coord-(crop_image_size/2)
+    top = y_coord-(crop_image_size/2)
+    right = left + crop_image_size
+    bottom =top + crop_image_size
+    imgSel = img.crop((left, top, right, bottom))
+    imgSel = imgSel.resize((224, 224), Image.LANCZOS)
+    imgSel = np.asarray(imgSel).reshape(1, 224, 224, 3)
+    image = preprocess_input(imgSel)
+    X_after_vgg = vgg16_model.predict(image)
+    X_after_vgg.shape
+    x = model.predict(X_after_vgg)
+    return x
 
 if __name__ == '__main__':
 
-
+    mat_file_dirs = list()
     for experiment_dir in os.listdir(basepath):
         experiment_dir += "/"
         print("processing: " + experiment_dir)
-        file_dirs.append(experiment_dir)
+        mat_file_dirs.append(experiment_dir)
+        evaluate_table(mat_file_dirs)
 
-    p = Pool()
-    result = p.map(evaluate_table, file_dirs)
-    p.close()
-    p.join()
+    
